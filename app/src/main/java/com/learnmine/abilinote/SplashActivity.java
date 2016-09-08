81718a506cd83601fbd5c21aabc8276bcbe5f49d
Cynthia Wasonga <wasonga.cynthia@yahoo.in>
2016-09-08 12:55:31 +0300

    Added the activities and layouts created

---
+++ app/src/main/java/com/learnmine/abilinote/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.learnmine.abilinote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by baa on 9/8/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
